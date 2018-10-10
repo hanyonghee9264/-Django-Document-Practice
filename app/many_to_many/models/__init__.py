04d6ffebffeeaa8dfb056cece382af9850996045
hanyonghee <hanyonghee9264@gmail.com>
2018-10-10 19:51:36 +0900

    many_to_many.models 패키지화

--- app/many_to_many/models/__init__.py
+++ app/many_to_many/models/__init__.py
@@ -1,19 +1,2 @@
-from django.db import models
-
-
-class Topping(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
-
-
-class Pizza(models.Model):
-    name = models.CharField(max_length=50)
-    toppings = models.ManyToManyField(
-        Topping,
-        related_name='pizzas',
-    )
-
-    def __str__(self):
-        return self.name
+from .basic import *
+from .intermediate import *
